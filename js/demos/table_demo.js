568318c13b6556037c38ee3252122022011a6861
abhineet <abhineet@abhineet-Inspiron-5521.(none)>
2014-11-05 15:06:15 +0530

    Table dummy implementation wholesalers

--- js/demos/table_demo.js
+++ js/demos/table_demo.js
@@ -2,11 +2,8 @@ $(function() {
   var table_1 = $('#table-1').dataTable ({
     "sAjaxSource": "./js/demos/table_data.json",
     "aoColumns": [
-      { "mData": "browser" },
-      { "mData": "engine" },
-      { "mData": "platform" },
-      { "mData": "version", "sClass": "text-center" },
-      { "mData": "grade", "sClass": "text-center" }
+      { "mData": "wholesaler_name" },
+      { "mData": "transportation_cost" }
     ],
     "fnInitComplete": function(oSettings, json) {
       $(this).parents ('.dataTables_wrapper').find ('.dataTables_filter input').prop ('placeholder', 'Table Search...').addClass ('form-control input-sm')
---
+++ wholesaler_show.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+  <title>Show Wholesalers &middot; Wholesalers &middot; Janta General Store</title>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Google Font: Open Sans -->
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,800,800italic">
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,300,700">
+
+  <!-- Font Awesome CSS -->
+  <link rel="stylesheet" href="./css/font-awesome.min.css">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="./css/bootstrap.min.css">
+
+  <!-- Plugin CSS -->
+  <link rel="stylesheet" href="./js/plugins/dataTables/dataTables.bootstrap.css">
+
+  <!-- App CSS -->
+  <link rel="stylesheet" href="./css/mvpready-admin.css">
+  <link rel="stylesheet" href="./css/mvpready-flat.css">
+  <!-- <link href="./css/custom.css" rel="stylesheet">-->
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="favicon.ico">
+
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+  <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+  <![endif]-->
+</head>
+
+<body class=" ">
+
+<div id="wrapper">
+  
+  <?php
+            include_once("navbar.html");
+  ?>
+  
+  <div class="content">
+
+    <div class="container">
+
+      <div class="portlet">
+
+        <h3 class="portlet-title">
+          <u>Show Wholesalers</u>
+        </h3>
+
+        <div class="portlet-body">
+
+          <table class="table table-striped table-bordered" id="table-1">
+            <thead>
+              <tr>
+                <th style="width: 30%">Wholesaler Name</th>
+                <th style="width: 20%">Transportation Cost</th>
+              </tr>
+            </thead>
+
+            <tfoot>
+              <tr>
+                <th>Wholesaler Name</th>
+                <th>Transportation Cost</th>
+              </tr>
+            </tfoot>
+          </table>
+
+        </div> <!-- /.portlet-body -->
+
+      </div> <!-- /.portlet -->
+
+    </div> <!-- /.container -->
+
+  </div> <!-- .content -->
+
+</div> <!-- /#wrapper -->
+
+<footer class="footer">
+  <div class="container">
+    <p class="pull-left">Copyright &copy; 2013 MVP Ready.</p>
+  </div>
+</footer>
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Core JS -->
+<script src="./js/libs/jquery-1.10.2.min.js"></script>
+<script src="./js/libs/bootstrap.min.js"></script>
+
+<!--[if lt IE 9]>
+<script src="./js/libs/excanvas.compiled.js"></script>
+<![endif]-->
+
+<!-- Plugin JS -->
+<script src="./js/plugins/dataTables/jquery.dataTables.js"></script>
+<script src="./js/plugins/dataTables/dataTables.bootstrap.js"></script>
+
+<!-- App JS -->
+<script src="./js/mvpready-core.js"></script>
+<script src="./js/mvpready-admin.js"></script>
+
+<!-- Plugin JS -->
+<script src="./js/demos/table_demo.js"></script>
+
+
+</body>
+</html>
