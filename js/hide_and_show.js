eb67b975a6e89640204a059d8fb385ddb8fa9829
Saksham Aggarwal <s.agg@live.com>
2014-11-06 22:36:16 +0530

    Fixed Issue #4

--- js/hide_and_show.js
+++ js/hide_and_show.js
@@ -1,9 +1,15 @@
 function hide_divs() {
 	$('.hide-and-show').hide();
+	document.getElementById('edu_qual').required = false;
+	document.getElementById('user_id').required = false;
+	document.getElementById('passwd').required = false;
 }
 
 function show_divs() {
 	$('.hide-and-show').show();
+	document.getElementById('edu_qual').required = true;
+	document.getElementById('user_id').required = true;
+	document.getElementById('passwd').required = true;
 }
 
 $(document).ready(
